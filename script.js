73b5618a24b5c15a352c1badc524188ff1573314
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-02-02 15:17:50 +0100

    Add replace and clear grid buttons

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
+let defaultDimension = 16;
+
 const gridContainer = document.querySelector('#grid-container');
-let gridDimension = 6;
+const replaceButton = document.querySelector('.replace')
+const clearButton = document.querySelector('.clear')
 
+const createGrid = (dimension) => {
+  for(i=0; i < dimension*dimension; i++) {
+    const pixel = document.createElement('div')
+    pixel.classList.add('pixel');
+    gridContainer.appendChild(pixel);
+    pixel.style.flex = '0 0 '+ (100/dimension)+'%';
+    pixel.addEventListener('mouseover', (e) => {
+      e.target.style.backgroundColor = 'blue';
+    })
+  }
+}
 
-for(i=0; i < gridDimension*gridDimension; i++) {
-  const pixel = document.createElement('div')
-  pixel.classList.add('pixel');
-  gridContainer.appendChild(pixel);
-  pixel.style.flex = '0 0 '+ (100/gridDimension)+'%';
-  pixel.addEventListener('mouseover', (e) => {
-    e.target.style.backgroundColor = 'blue';
+const replaceGrid = () => {
+  let dim = parseInt(window.prompt('DIME'))
+  if (dim < 1 || dim > 64) {
+    alert('AGGIUNGI UNA DIMENSONE VALIDA')
+    dim = defaultDimension;
+  }
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((square => {
+    gridContainer.removeChild(square)
+  }))
+  createGrid(dim);
+}
+
+const clearGrid = () => {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((square) => {
+    square.style.removeProperty('background-color');
   })
 }
 
+
+
+
+createGrid(defaultDimension);
+
+replaceButton.addEventListener('click', replaceGrid)
+clearButton.addEventListener('click', clearGrid)
 
 
 
